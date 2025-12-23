aa98b33d340213d14b43c6994bc52afa65c24fd8
yaroslav8765 <pechorkin2014@gmail.com>
2025-12-23 16:47:12 +0200

    fix: update API call to use simplified path and include resourceId in request body

--- index.ts
+++ index.ts
@@ -11,8 +11,11 @@ import { interpretResource, ActionCheckSource } from "adminforth";
 
 export default class ForeignInlineListPlugin extends AdminForthPlugin {
   foreignResource: AdminForthResource;
+  copyOfForeignResource: AdminForthResource;
   options: PluginOptions;
   adminforth: IAdminForth;
+
+  activationOrder: number = -10000000;
 
   constructor(options: PluginOptions) {
     super(options, import.meta.url);
@@ -24,81 +27,7 @@ export default class ForeignInlineListPlugin extends AdminForthPlugin {
   }
 
   setupEndpoints(server: IHttpServer) {
-    process.env.HEAVY_DEBUG && console.log(`🪲 ForeignInlineListPlugin.setupEndpoints, registering: '/plugin/${this.pluginInstanceId}/get_resource'`);
-    server.endpoint({
-      method: 'POST',
-      path: `/plugin/${this.pluginInstanceId}/get_resource`,
-      handler: async ({ body, adminUser }) => {
-        const resource = this.adminforth.config.resources.find((res) => this.options.foreignResourceId === res.resourceId);
-        if (!resource) {
-          return { error: `Resource ${this.options.foreignResourceId} not found` };
-        }
-        // exclude "plugins" key
-        const resourceCopy = clone({ ...resource, plugins: undefined });
-
-        if (this.options.modifyTableResourceConfig) {
-          this.options.modifyTableResourceConfig(resourceCopy);
-        }
-
-        const { allowedActions } = await interpretResource(adminUser, resourceCopy, {}, ActionCheckSource.DisplayButtons, this.adminforth);
-
-        return { 
-          resource: { 
-            ...resourceCopy,
-            options: {
-              ...resourceCopy.options,
-              allowedActions,
-            },
-          }
-        };
-      }
-    });
-    server.endpoint({
-      method: 'POST',
-      path: `/plugin/${this.pluginInstanceId}/start_bulk_action`,
-      handler: async ({ body, adminUser, tr }) => {
-          const { resourceId, actionId, recordIds } = body;
-          const resource = this.adminforth.config.resources.find((res) => res.resourceId == resourceId);
-          if (!resource) {
-              return { error: await tr(`Resource {resourceId} not found`, 'errors', { resourceId }) };
-          }
-
-          const resourceCopy = JSON.parse(JSON.stringify({ ...resource, plugins: undefined }));
-
-
-          if (this.options.modifyTableResourceConfig) {
-            this.options.modifyTableResourceConfig(resourceCopy);
-          }
-          
-          const { allowedActions } = await interpretResource(
-            adminUser, 
-            resourceCopy, 
-            { requestBody: body },
-            ActionCheckSource.BulkActionRequest,
-            this.adminforth
-          );
-
-          const action = resourceCopy.options.bulkActions.find((act) => act.id == actionId);
-          if (!action) {
-            return { error: await tr(`Action {actionId} not found`, 'errors', { actionId }) };
-          } 
-          
-          if (action.allowed) {
-            const execAllowed = await action.allowed({ adminUser, resourceCopy, selectedIds: recordIds, allowedActions });
-            if (!execAllowed) {
-              return { error: await tr(`Action "{actionId}" not allowed`, 'errors', { actionId: action.label }) };
-            }
-          }
-          const response = await action.action({selectedIds: recordIds, adminUser, resourceCopy, tr});
-          
-          return {
-            actionId,
-            recordIds,
-            resourceId,
-            ...response
-          }
-      }
-    })
+    console.log("Setting up endpoints for plugin", this.pluginInstanceId);
     server.endpoint({
       method: 'POST',
       path: `/plugin/${this.pluginInstanceId}/get_default_filters`,
@@ -122,20 +51,15 @@ export default class ForeignInlineListPlugin extends AdminForthPlugin {
   async modifyResourceConfig(adminforth: IAdminForth, resourceConfig: AdminForthResource) {
     super.modifyResourceConfig(adminforth, resourceConfig);
     this.adminforth = adminforth;
-
-    // get resource with foreignResourceId
     this.foreignResource = adminforth.config.resources.find((resource) => resource.resourceId === this.options.foreignResourceId);
+
     if (!this.foreignResource) {
       const similar = suggestIfTypo(adminforth.config.resources.map((res) => res.resourceId), this.options.foreignResourceId);
       throw new Error(`ForeignInlineListPlugin: Resource with ID "${this.options.foreignResourceId}" not found. ${similar ? `Did you mean "${similar}"?` : ''}`);
     }
-    if (this.foreignResource.resourceId === resourceConfig.resourceId) {
-      throw new Error(`TEMPORARY ERROR ForeignInlineListPlugin: foreignResourceId "${this.options.foreignResourceId}" cannot be the same as the resource where the plugin is installed ("${resourceConfig.resourceId}")`);
-    }
     const idOfNewCopy = `${this.foreignResource.resourceId}_inline_list__from_${this.resourceConfig.resourceId}__`;
     
 
-    const defaultSort = this.foreignResource.options?.defaultSort;
     const newColumn = {
       name: `foreignInlineList_${this.foreignResource.resourceId}`,
       label: 'Foreign Inline List',
@@ -155,15 +79,7 @@ export default class ForeignInlineListPlugin extends AdminForthPlugin {
             ...this.options, 
             pluginInstanceId: this.pluginInstanceId,
             disableForeignListResourceRefColumn: this.options.disableForeignListResourceRefColumn,
-            ...(defaultSort
-              ? {
-                  defaultSort: {
-                    field: defaultSort.columnName,
-                    direction: defaultSort.direction,
-                  }
-                }
-              : {}
-            )
+            foreignResourceId: idOfNewCopy
           }
         }
       },
@@ -208,7 +124,6 @@ export default class ForeignInlineListPlugin extends AdminForthPlugin {
     }
 
     // get resource with foreignResourceId
-    console.log('Creating copy of foreign resource', this.foreignResource.resourceId, 'as', idOfNewCopy);
     this.copyOfForeignResource = clone({ ...this.foreignResource, plugins: [] });
 
     // if we install on plugin which is already a copy, adjust foreignResource references
@@ -245,15 +160,9 @@ export default class ForeignInlineListPlugin extends AdminForthPlugin {
     for (const plugin of this.copyOfForeignResource.plugins.sort((a, b) => a.activationOrder - b.activationOrder)) {
       // if there already is a plugin with same instanceUniqueRepresentation, skip
       if (plugin.modifyResourceConfig) {
-        await plugin.modifyResourceConfig(adminforth, this.copyOfForeignResource);
+        plugin.modifyResourceConfig(adminforth, this.copyOfForeignResource);
       }
-      console.log(`🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮Activating plugin ${plugin.pluginInstanceId} for resource ${this.copyOfForeignResource.resourceId}`);
-      if (plugin.setupEndpoints) {
-        await plugin.setupEndpoints(adminforth.express);
-      }
-      if (plugin.validateConfigAfterDiscover) {
-        await plugin.validateConfigAfterDiscover(adminforth, this.copyOfForeignResource);
-      }
+      this.adminforth.activatedPlugins.push(plugin);
     }
 
 
